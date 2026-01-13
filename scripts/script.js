5bbf99c691f1a3c512b0fe1d7ff8828d293bb253
Rowan <48519289+scar055@users.noreply.github.com>
2026-01-13 14:38:48 +0100

    add js interaction #5

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//stap 1 select the hamburger menu button
+const openMenu = document.querySelector("button");
+//stap 2 add a click event
+//stap 3 add class to the navl list to make it vissible
+openMenu.addEventListener("click", function () {
+  document.querySelector("ul").classList.toggle("vissible");
+});
