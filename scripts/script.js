55c5b837dca602e2d5e293fb95d38641afb563b4
Rowan <48519289+scar055@users.noreply.github.com>
2026-01-14 11:40:22 +0100

    add animation to menu #5

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,5 @@ const openMenu = document.querySelector("button");
 //stap 3 add class to the navl list to make it vissible
 openMenu.addEventListener("click", function () {
   document.querySelector("ul").classList.toggle("vissible");
+  document.querySelector("body").classList.toggle("scroll");
 });
